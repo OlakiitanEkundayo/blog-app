bb858dbdae5cc0ad62dada5957602818d01a9c94
Olakiitan <ekundayoopeyemi123@gmail.com>
2025-06-13 22:28:25 +0100

    feat: implement post deletion logic

--- admin/delete-post.php
+++ admin/delete-post.php
@@ -1,3 +1,11 @@
 <?php
 require_once __DIR__ . '/../includes/db.inc.php';
 require_once __DIR__ . '/../includes/function.inc.php';
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $stmt = $pdo->prepare("DELETE FROM `blog_table` WHERE id = ?");
+    $stmt->execute([$id]);
+    header('Location: all-posts.php?deleted=true');
+    exit;
+}
